4686f1ec7ce773fae3761eb578bc9531ff0dd959
McDonald <mus22003@byui.edu>
2024-02-07 10:44:05 +0200

    updated header
    Added sign in button and cart

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <div>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Next Fruitify
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">Cart</Link>
+            </li>
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">Sign in</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
